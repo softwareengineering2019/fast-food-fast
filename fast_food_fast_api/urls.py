a97739776341a4104f3d48284dd07110c9a34189
james kisuule <getjames20@gmail.com>
2018-09-14 12:10:41 +0300

    integrated api

---
+++ fast_food_fast_api/urls.py
@@ -0,0 +1,21 @@
+
+"""
+This module assigns views to urls
+"""
+from views import GetOrder
+
+class GetUrls:
+    """
+    Method that views with urls
+    """
+    @staticmethod
+    def fetch_urls(app):
+        """
+        Method that views with urls
+        """
+        orders_view = GetOrder.as_view('orders')
+        app.add_url_rule('/api/v1/orders', view_func=orders_view,
+                         defaults={'order_id': None}, methods=['GET'])
+        app.add_url_rule('/api/v1/orders/<int:order_id>', view_func=orders_view, methods=['GET'])
+        app.add_url_rule('/api/v1/orders', view_func=orders_view, methods=['POST'])
+        app.add_url_rule('/api/v1/orders/<int:id>', view_func=orders_view, methods=['PUT'])
