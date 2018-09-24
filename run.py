056c812dfae202620da2cbbce94978ede841e0de
james kisuule <getjames20@gmail.com>
2018-09-24 22:26:36 +0300

    Add run.py file for running the app

---
+++ run.py
@@ -0,0 +1,13 @@
+"""import the flask name from the api folder"""
+from api import APP
+from api.urls import GetUrls
+"""
+module for running the application
+"""
+APP.env = 'development'
+APP.testing = True
+GetUrls.fetch_urls(APP)
+
+if __name__ == '__main__':
+    """ This keeps the serve running"""
+    APP.run(debug=True)
