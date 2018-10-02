ec2459cb01fea7da72006e59c0eb7b22e8301f82
james kisuule <getjames20@gmail.com>
2018-10-02 21:56:55 +0300

    add tests

--- api/tests/test_case.py
+++ api/tests/test_case.py
@@ -1,45 +1,41 @@
+"""
+    Module for making tests on the app for sign up
+"""
 import unittest
 import json
+import psycopg2
+from run import APP
 from api.controller.login import LoginUsers
 from api.controller.register import RegisterUsers
-# from db_connection.create_tables import config
+import os
 
-# from app.models.models import drop, initialize
-from connect import APP
-
-
-class LoginRegisterTestCase(unittest.TestCase):
+class TestViews(unittest.TestCase):
+    """"
+        Class for testing  signing up
+        params: unittest.testCase
+    """
 
     def setUp(self):
-        self.client = APP.test_client(self)
-        user={
-        "name": "james",
-        "email": "james@gmail.com",
-        "phone":"0784909440",
-        "password": "james"
-        }
+        """
+           Method for making the client object
+        """
+        # self.client = APP.test_client
+        APP.config['TESTING'] = True
+        self.app = APP
+        self.client = APP.test_client
+        # GetAllOrder.__init__(APP) 
+    def test_sign(self):
+        """
+            Method for testing the post function which adds new user
+        """
+        result = self.client().post('/api/v2/auth/signup',
+                                    content_type="application/json",
+                                    data=json.dumps(dict(name="moses", email="moses@gmail.com",phone="0764324040",
+                                                         password="moses")))
+        respond = json.loads(result.data.decode("utf8"))
+        self.assertIn('message', respond)
+        self.assertIsInstance(respond, dict)
+        self.assertEqual(result.status_code, 201)
+        self.assertTrue(result.json["message"])
 
-    def test_user_can_login(self):
-        '''This tests whether a registered user can login'''
-        result = self.client.post('/api/v2/auth/signup', data=json.dumps(self.user, content_type='application/json'))
-
-        # result  = self.client.post(
-            # '/api/v2/auth/login', data=json.dumps(self.data["login"]), content_type='application/json')
-        self.assertEqual(result.status_code, 200)
-        # self.assertTrue(b'Login successfull!', res.data)
-
-    # def test_wrong_password(self):
-    #     '''This tests whether a user cannot login with a wrong password'''
-    #     result  = self.client.post(
-    #         '/api/v2/auth/signup', data=json.dumps(self.data["user"]), content_type='application/json')
-    #     result  = self.client.post(
-    #         '/api/v2/auth/login', data=json.dumps(self.data["wrong_password"]), content_type='application/json')
-    #     self.assertEqual(res.status_code, 401)
-    #     self.assertTrue(b'password incorrect!', res.data)
-
-    # def test_unknown_user_cannot_login(self):
-    #     '''This tests whether an unknown user cannot login'''
-    #     result  = self.client.post(
-    #         '/api/v2/auth/login', data=json.dumps(self.data["unknown"]), content_type='application/json')
-    #     self.assertEqual(res.status_code, 400)
-    #     self.assertTrue(b'User does not exist!', res.data)
+    
